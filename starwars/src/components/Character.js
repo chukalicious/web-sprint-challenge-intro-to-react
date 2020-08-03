52b0508af016b24884b8c4a6949a0e31aea84f36
Katiuska <katiuskaalicea@gmail.com>
2020-08-02 20:48:35 -0700

    Styled headers in cards

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -9,10 +9,23 @@ import styled from "styled-components"
 const CharacterCard = styled.div`
     display: flex; 
     flex-direction: column; 
+    width: 65%; 
+    margin: 2rem auto; 
+    border-radius: .3rem; 
+    padding: .5rem; 
+    background-color: white; 
+`
+const Name = styled.h2`
+    background-color: #ECDFD2;
+    text-align: left; 
+    margin: -.5rem -.5rem; 
+    padding: .5rem 1rem 1rem 0rem;  
+
 `
 
 const Line = styled.p`
     margin: .2rem; 
+    padding-top: 1rem; 
 `
 
 const Character = (props) => {
@@ -20,7 +33,7 @@ const Character = (props) => {
     return(
 
         <CharacterCard>
-            <h2>{props.name}</h2>
+            <Name>{props.name}</Name>
             <Line>Gender: {props.gender.toUpperCase()}</Line>
             <Line>Birth Year: {props.birthYear}</Line>
             <Line>Height: {props.height}</Line>
